271a8d9f990cea6b59ae25d8f6d46b7f3e7646b5
Veries Seals <veries@me.com>
2025-03-23 21:17:56 -0500

    Added int high score variable to calculate score

---
+++ src/MainChallenge.java
@@ -0,0 +1,42 @@
+public class MainChallenge {
+
+    // This is actually the first method below. Every thing included in public static void main(String[] args) throws Exception {}
+    // --------------------------------------------------------------
+    public static void main(String[] args) {
+        
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+        
+        // Call the 2nd Method created
+        // --------------------------------------------------------------
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus); // Implement arguments made from parameters set in Method 2. Variable name and value will make up the arguments here.
+        System.out.println("The highScore is " + highScore);
+
+        // Do not redeclair variable, just change them on this step
+        // --------------------------------------------------------------
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        calculateScore(gameOver, score, levelCompleted, bonus);
+
+    }
+    
+    // Create our 2nd method below the first method
+    // --------------------------------------------------------------
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) { // add the parameters (variable type and name separated by commas)
+        
+        int finalScore = score;
+        
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+        }
+
+        return {
+            finalScore;
+        }
+    }
+}
